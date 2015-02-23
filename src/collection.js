a5f30f78b83698b295d9ee585f985aaeddeb23ca
Nathan Borror <nathan@nathanborror.com>
2015-02-22 17:59:53 -0800

    Making Collection a dumb container for now.

--- src/collection.js
+++ src/collection.js
@@ -20,23 +20,10 @@ var Section = React.createClass({
 });
 
 var Collection = React.createClass({
-  getInitialState: function() {
-    return {data: []};
-  },
-  componentDidMount: function() {
-    // Implement
-  },
-  handleClick: function() {
-    // Implement
-  },
   render: function() {
-    var cells = this.state.data.map(function(obj) {
-      return <Cell key={obj.key} onClick={this.handleClick}>{obj.title}</Cell>
-    }.bind(this));
-
     return (
       <div className='bp-collection'>
-        {cells}
+        {this.props.children}
       </div>
     );
   }
